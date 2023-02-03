4deadd5208e3ad681429e3ac7af7aedb437ec317
Antonio Luis Garcia <antonlugar777@gmail.com>
2023-02-03 23:35:59 +0100

    generador de archivo contador de palabras

--- practices/wordsCounter.java
+++ practices/wordsCounter.java
@@ -4,6 +4,7 @@ public class wordsCounter {
     public static void main(String[] args) {
         WordCounter w = new WordCounter(args[0] , args[1]);
         w.readText();
+        w.generateWordCounter();
         System.out.println(w);
     }
 }
