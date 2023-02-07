6edd9c51dff9cd0af4afb69412f166ea2f3511cb
Antonio Luis Garcia <antonlugar777@gmail.com>
2023-02-07 17:32:12 +0100

    borrado de importaciones sobrantes y mas spaceado

--- estadisticas/FileStats.java
+++ estadisticas/FileStats.java
@@ -1,8 +1,6 @@
 package estadisticas;
 
-import java.io.BufferedWriter;
 import java.io.BufferedReader;
-import java.io.FileWriter;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -29,7 +27,9 @@ public class FileStats {
             while( (line=br.readLine()) != null){
                 list.add(line);
             }
+
             br.close();
+
         }catch(IOException io){
             System.out.println(io.getMessage());
         }
@@ -45,15 +45,20 @@ public class FileStats {
             String line = "";
 
             while( (line=br.readLine()) != null){
+
                 line = reduceBlanks(replaceIfNotAlphabetic(line, ' '));
                 String[] words = line.split(" ");
+
                 for(int i = 0 ; i < words.length; i ++){
+
                     if(words[i] != null && !words.equals("")){
                         list.add(words[i]);
                     }
                 }
             }
+
             br.close();
+
         }catch(IOException io){
             System.out.println(io.getMessage());
         }
@@ -62,11 +67,14 @@ public class FileStats {
     }
 
     private static String replaceIfNotAlphabetic(String text, char replaceBy){
-        for(int i = 0 ; i< text.length(); i++){
+
+        for(int i = 0 ; i < text.length() ; i++){
+
             if(!Character.isAlphabetic(text.charAt(i))){
                 text = text.replace(text.charAt(i),replaceBy);
             }
         }
+
         return text;
     }
 
@@ -75,6 +83,7 @@ public class FileStats {
     }
 
     public ArrayList<Word> getDifferentWords(){
+
         HashMap<String,Word> wordsAuxiliar = new HashMap<>();
         ArrayList<Word> words = new ArrayList<>();
 
@@ -83,10 +92,14 @@ public class FileStats {
             String line = "";
 
             while( (line=br.readLine()) != null){
+
                 line = reduceBlanks(replaceIfNotAlphabetic(line, ' '));
                 String[] wordsArray = line.split(" ");
+
                 for(int i = 0 ; i < wordsArray.length; i ++){
+
                     if(wordsArray[i] != null && !wordsArray.equals("")){
+                        
                         if(wordsAuxiliar.containsKey(wordsArray[i])){
                             Word w = wordsAuxiliar.get(wordsArray[i]);
                             w.inc();
